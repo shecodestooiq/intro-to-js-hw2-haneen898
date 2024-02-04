f9910175343ac52a239647c877f410421b501cc8
haneen898 <haneenmansoor898@gmail.com>
2024-02-04 21:27:05 +0300

    final

---
+++ index.js
@@ -0,0 +1,11 @@
+var a = 35
+var b = 60 
+
+console.log ("a greater than b" , a > b) 
+console.log("a is less than b", a < b) 
+console.log("greater or equal", a >= b) 
+console.log("equal", a == b) 
+console.log("less or equal" , a <= b)
+console.log("NOT", a != b )
+
+ 
